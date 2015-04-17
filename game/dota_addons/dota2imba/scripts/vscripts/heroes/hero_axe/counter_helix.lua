c2744d30543b9ec3983dcac23022fc1a1352e6c2
XavierCHN <364671459@qq.com>
2015-04-18 01:00:35 +0800

    完成- 斧王-BANE BANE大招未做

--- game/dota_addons/dota2imba/scripts/vscripts/heroes/hero_axe/counter_helix.lua
+++ game/dota_addons/dota2imba/scripts/vscripts/heroes/hero_axe/counter_helix.lua
@@ -9,6 +9,16 @@ function CounterHelix( keys )
 	local ability = keys.ability
 	local helix_modifier = keys.helix_modifier
 
+	local trigger_chance_base = ability:GetLevelSpecialValueFor("trigger_chance", ability:GetLevel() - 1)
+	local trigger_chance_per_agi =  ability:GetLevelSpecialValueFor("trigger_chance_per_agi", ability:GetLevel() - 1)
+	local trigger_chance_bonus = caster:GetAgility() * trigger_chance_per_agi
+	local random_val = RandomInt(0,100)
+
+	if ( random_val < trigger_chance_base + trigger_chance_bonus) then
+		print("counter helix not triggered", random_val, trigger_chance_base , trigger_chance_bonus )
+		return
+	end
+
 	-- If the caster has the helix modifier then do not trigger the counter helix
 	-- as its considered to be on cooldown
 	if target == caster and not caster:HasModifier(helix_modifier) then
